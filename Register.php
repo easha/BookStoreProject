e23a449e5be30f945e7b1a1d30cffe6b313cd4ec
easha <farlinabarikeasha@gmail.com>
2018-07-31 06:13:46 +0600

    Add files via upload
    this is the updated register file..dont use the previous one

--- Register.php
+++ Register.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>My first PHP website</title>
+		<title>Bookworms</title>
 	</head>
 	<body>
 		<h2>Registration Page</h2>
@@ -21,34 +21,36 @@ Enter Phone number: <input type="text" name="phn" required="required" /> <br/>
 </html>
 
 <?php
+$link = mysqli_connect("localhost", "root", "") or die($link);
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$username = mysql_real_escape_string($_POST['username']);
-	$password = mysql_real_escape_string($_POST['password']);
-	$email = mysql_real_escape_string($_POST['email']);
-	$address = mysql_real_escape_string($_POST['address']);
- 	$fname = mysql_real_escape_string($_POST['fname']);
-	$lanme = mysql_real_escape_string($_POST['lname']);
-	$phn = mysql_real_escape_string($_POST['phn']);
+	$username = mysqli_real_escape_string($link,$_POST['username']);
+	$password = mysqli_real_escape_string($link,$_POST['password']);
+	$email = mysqli_real_escape_string($link,$_POST['email']);
+	$address = mysqli_real_escape_string($link,$_POST['address']);
+ 	$fname = mysqli_real_escape_string($link,$_POST['fname']);
+	$lanme = mysqli_real_escape_string($link,$_POST['lname']);
+	$phn = mysqli_real_escape_string($link,$_POST['phn']);
 
     $bool = true;
-	mysql_connect("localhost", "root","") or die(mysql_error()); //Connect to server
-	mysql_select_db("bookworm") or die("Cannot connect to database"); //Connect to database
-	$query = mysql_query("Select * from Customers"); //Query the users table
-	while($row = mysql_fetch_array($query)) //display all rows from query
+mysqli_select_db($link,"bookworm") or die("Cannot connect to database"); //Connect to database
+	//mysqli_connect("localhost", "root","") or die(mysql_error()); //Connect to server
+	//mysqli_select_db("bookworm") or die("Cannot connect to database"); //Connect to database
+	$query = mysqli_query("Select * from Customers"); //Query the users table
+	while($row = mysqli_fetch_array($query))
 	{
-		$table_users = $row['User_name']; // the first username row is passed on to $table_users, and so on until the query is finished
+		$table_users = $row['User_name']; 
 		if($username == $table_User_name) // checks if there are any matching fields
 		{
-			$bool = false; // sets bool to false
-			Print '<script>alert("Username has been taken!");</script>'; //Prompts the user
-			Print '<script>window.location.assign("Register.php");</script>'; // redirects to register.php
+			$bool = false; 
+			Print '<script>alert("Username has been taken!");</script>'; 
+			Print '<script>window.location.assign("Register.php");</script>'; 
 		}
 	}
-	if($bool) // checks if bool is true
+	if($bool) 
 	{
-		mysql_query("INSERT INTO Customers (User_name,Password,First_name,Last_name,Email,Address,Phone_number) VALUES ('$username','$password','$fname','$lname','$email','$address','$phn')"); //Inserts the value to table users
-		Print '<script>alert("Successfully Registered!");</script>'; // Prompts the user
-		Print '<script>window.location.assign("Register.php");</script>'; // redirects to register.php
+		mysqli_query($link,"INSERT INTO Customers (User_name,Password,First_name,Last_name,Email,Address,Phone_number) VALUES ('$username','$password','$fname','$lname','$email','$address','$phn')"); 
+		Print '<script>alert("Successfully Registered!");</script>'; 
+		Print '<script>window.location.assign("Register.php");</script>'; 
 	}
 }
 ?>
