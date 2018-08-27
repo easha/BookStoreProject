f1424afa89f6f93772a074cb2a40629fe9f31503
easha <farlinabarikeasha@gmail.com>
2018-08-27 08:21:10 +0600

    final sql file

--- bookworm.sql
+++ bookworm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2018 at 06:15 PM
+-- Generation Time: Aug 27, 2018 at 04:18 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -63,13 +63,21 @@ CREATE TABLE `Books` (
   `Author_id` int(11) NOT NULL,
   `Publisher_id` int(11) NOT NULL,
   `Title` varchar(255) NOT NULL,
-  `Catagory` varchar(255) NOT NULL,
+  `Category` varchar(255) NOT NULL,
   `Publication_year` int(11) NOT NULL,
   `Price` double NOT NULL,
   `Condition` varchar(255) NOT NULL,
   `Quantity` int(11) NOT NULL,
   `About` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Books`
+--
+
+INSERT INTO `Books` (`Book_id`, `Author_id`, `Publisher_id`, `Title`, `Category`, `Publication_year`, `Price`, `Condition`, `Quantity`, `About`) VALUES
+(3, 2, 3, 'splendid sun', 'horror', 2000, 90, 'good', 34, 'tragic'),
+(4, 8, 7, 'me before u', 'romance', 1999, 890, 'good', 76, 'kissu bolar nai');
 
 -- --------------------------------------------------------
 
@@ -78,12 +86,18 @@ CREATE TABLE `Books` (
 --
 
 CREATE TABLE `Card` (
-  `Customer_id` int(11) NOT NULL,
-  `Card_type` varchar(255) NOT NULL,
-  `PIN` int(11) NOT NULL,
   `Card_number` int(11) NOT NULL,
-  `User_name` varchar(255) NOT NULL
+  `User_name` varchar(255) NOT NULL,
+  `Customer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Card`
+--
+
+INSERT INTO `Card` (`Card_number`, `User_name`, `Customer_id`) VALUES
+(121212, 'lllll', 5),
+(2147483647, 'lllll', 5);
 
 -- --------------------------------------------------------
 
@@ -99,20 +113,19 @@ CREATE TABLE `Customers` (
   `Address` varchar(255) NOT NULL,
   `Phone_number` varchar(255) NOT NULL,
   `User_name` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Type` varchar(255) NOT NULL
+  `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Customers`
 --
 
-INSERT INTO `Customers` (`Customer_id`, `First_name`, `Last_name`, `Email`, `Address`, `Phone_number`, `User_name`, `Password`, `Type`) VALUES
-(1, 'nn', '', 'nbjh', 'jmnk', 'j', 'rr', '66', ''),
-(2, 'jzhjkxfg', '', 'kfjhj', ',zndgj', '76', 'ww', '33', ''),
-(3, 'nbj', '', 'jhg', 'jhjkjhj', '786', 'lll', '000', ''),
-(4, 'iwui', 'lwiu', 'jagSD', 'kuhweu', '786', 'oo', '88', ''),
-(5, 'kqjhjfj', 'jhef', 'wehf', 'ljaf', '8yuery', 'kkk', '77', '');
+INSERT INTO `Customers` (`Customer_id`, `First_name`, `Last_name`, `Email`, `Address`, `Phone_number`, `User_name`, `Password`) VALUES
+(5, 'kqjhjfj', 'jhef', 'wehf', 'ljaf', '8yuery', 'kkk', '77'),
+(6, 'fgg', 'drg', 'ydghiuyo', 'fdgh', 'rtg', 'lllll', 'rtrt'),
+(7, 'kwerj', 'ermgn', 'kerg', 'lkwerj', '65', 'we', '23'),
+(8, 'r', 'g', 'dfgm,n', ',fgn', '4', 'rrrr', 'rrrr'),
+(9, 'j', 'y', 'jkh', 'j', '0', 'oo', '00');
 
 -- --------------------------------------------------------
 
@@ -146,20 +159,27 @@ CREATE TABLE `Publisher` (
 
 CREATE TABLE `Recommendation` (
   `Customer_id` int(11) NOT NULL,
-  `Horror` int(11) NOT NULL,
+  `Horror` int(11) DEFAULT NULL,
   `Cooking` int(11) NOT NULL,
   `Liturature` int(11) NOT NULL,
-  `Action` int(11) NOT NULL,
   `Mystery` int(11) NOT NULL,
   `Biography` int(11) NOT NULL,
   `History` int(11) NOT NULL,
   `Religion` int(11) NOT NULL,
-  `Romance` int(11) NOT NULL,
-  `Science fiction` int(11) NOT NULL,
+  `Romance` int(11) DEFAULT NULL,
   `Poetry` int(11) NOT NULL,
   `Comic` int(11) NOT NULL,
-  `Manga` int(11) NOT NULL
+  `Manga` int(11) NOT NULL,
+  `Adventure` int(11) NOT NULL,
+  `Science_fiction` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Recommendation`
+--
+
+INSERT INTO `Recommendation` (`Customer_id`, `Horror`, `Cooking`, `Liturature`, `Mystery`, `Biography`, `History`, `Religion`, `Romance`, `Poetry`, `Comic`, `Manga`, `Adventure`, `Science_fiction`) VALUES
+(6, 109, 2, 0, 0, 0, 0, 0, 44, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -171,8 +191,20 @@ CREATE TABLE `Review` (
   `Customer_id` int(11) NOT NULL,
   `Review` varchar(500) NOT NULL,
   `Book_id` int(11) NOT NULL,
-  `Title` varchar(255) NOT NULL
+  `Title` varchar(255) NOT NULL,
+  `Type` varchar(255) NOT NULL,
+  `Rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Review`
+--
+
+INSERT INTO `Review` (`Customer_id`, `Review`, `Book_id`, `Title`, `Type`, `Rating`) VALUES
+(6, 'good', 77, 'splendid sun', 'horror', 5),
+(6, 'good', 77, 'lol', 'horror', 3),
+(6, 'good', 77, 'vut', 'horror', 1),
+(6, 'good', 77, 'spl', 'romance', 2);
 
 -- --------------------------------------------------------
 
@@ -217,7 +249,7 @@ ALTER TABLE `Books`
 -- Indexes for table `Card`
 --
 ALTER TABLE `Card`
-  ADD PRIMARY KEY (`Card_number`,`PIN`),
+  ADD PRIMARY KEY (`Card_number`),
   ADD KEY `Customer_id` (`Customer_id`);
 
 --
@@ -281,13 +313,13 @@ ALTER TABLE `Author`
 -- AUTO_INCREMENT for table `Books`
 --
 ALTER TABLE `Books`
-  MODIFY `Book_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `Customers`
 --
 ALTER TABLE `Customers`
-  MODIFY `Customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `Order_item`
@@ -312,13 +344,6 @@ ALTER TABLE `Author`
   ADD CONSTRAINT `Author_ibfk_1` FOREIGN KEY (`Book_id`) REFERENCES `Books` (`Book_id`);
 
 --
--- Constraints for table `Books`
---
-ALTER TABLE `Books`
-  ADD CONSTRAINT `Books_ibfk_1` FOREIGN KEY (`Publisher_id`) REFERENCES `Publisher` (`Publisher_id`),
-  ADD CONSTRAINT `Foreign key constraints` FOREIGN KEY (`Author_id`) REFERENCES `Author` (`Author_id`);
-
---
 -- Constraints for table `Card`
 --
 ALTER TABLE `Card`
@@ -341,13 +366,6 @@ ALTER TABLE `Publisher`
 --
 ALTER TABLE `Recommendation`
   ADD CONSTRAINT `Recommendation_ibfk_1` FOREIGN KEY (`Customer_id`) REFERENCES `Customers` (`Customer_id`);
-
---
--- Constraints for table `Review`
---
-ALTER TABLE `Review`
-  ADD CONSTRAINT `Review_ibfk_1` FOREIGN KEY (`Customer_id`) REFERENCES `Customers` (`Customer_id`),
-  ADD CONSTRAINT `Review_ibfk_2` FOREIGN KEY (`Book_id`) REFERENCES `Books` (`Book_id`);
 
 --
 -- Constraints for table `Suggestion`
